7eb78ffdd2db67a50e6e140ef94a98e1fb9ac5f6
Birol COŞGUN <142435002+BirolStudio@users.noreply.github.com>
2025-06-25 13:32:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// categories.js dosyası
+const categories = [
+    { value: '', text: 'Tüm Kategoriler' },
+    { value: 'Korku', text: 'Korku' },
+    { value: 'Macera', text: 'Macera' },
+    { value: 'FPS', text: 'FPS' },
+    { value: '2D', text: '2D' },
+    { value: '3D', text: '3D' },
+    { value: 'Tek Oyunculu', text: 'Tek Oyunculu' },
+    { value: 'Çok Oyunculu', text: 'Çok Oyunculu' },
+    { value: 'Strateji', text: 'Strateji' },
+    { value: 'İki Kişilik', text: 'İki Kişilik' },
+    { value: 'Simülasyon', text: 'Simülasyon' },
+    { value: 'Kasa Açma', text: 'Kasa Açma' },
+    { value: 'Pixel Art', text: 'Pixel Art' },
+    { value: 'Skor Tablosu', text: 'Skor Tablosu' }
+];
+
+
+function updateCategories() {
+        const kategoriSelect = document.getElementById('kategoriSelect');
+        kategoriSelect.innerHTML = ''; // Önce mevcut seçenekleri temizle
+
+        categories.forEach(category => {
+            const option = document.createElement('option');
+            option.value = category.value;
+            option.textContent = category.text;
+            kategoriSelect.appendChild(option);
+        });
+    }
+
+    // Sayfa yüklendiğinde kategorileri güncelle
+    document.addEventListener('DOMContentLoaded', function() {
+        updateCategories(); // Kategorileri dinamik olarak ekle
+    });
